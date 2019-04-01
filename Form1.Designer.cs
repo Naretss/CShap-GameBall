545301ba47af48627ca226124e088a8213ae2a91
naretss <pu_naret@hotmail.com>
2019-04-01 16:05:59 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.slider = new System.Windows.Forms.PictureBox();
+            this.vert = new System.Windows.Forms.Timer(this.components);
+            this.horz = new System.Windows.Forms.Timer(this.components);
+            this.key_watch = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_score = new System.Windows.Forms.Label();
+            this.label_sum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Enabled = false;
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(38, 9);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(50, 50);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(848, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // slider
+            // 
+            this.slider.BackColor = System.Drawing.Color.Orange;
+            this.slider.Location = new System.Drawing.Point(270, 486);
+            this.slider.Name = "slider";
+            this.slider.Size = new System.Drawing.Size(127, 13);
+            this.slider.TabIndex = 2;
+            this.slider.TabStop = false;
+            // 
+            // vert
+            // 
+            this.vert.Enabled = true;
+            this.vert.Tick += new System.EventHandler(this.vert_Tick_1);
+            // 
+            // horz
+            // 
+            this.horz.Enabled = true;
+            this.horz.Tick += new System.EventHandler(this.horz_Tick);
+            // 
+            // key_watch
+            // 
+            this.key_watch.Enabled = true;
+            this.key_watch.Tick += new System.EventHandler(this.key_watch_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel1.Controls.Add(this.label_sum);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(53, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(740, 301);
+            this.panel1.TabIndex = 3;
+            this.panel1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(196, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(351, 55);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "GAME PAUSE";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label_score
+            // 
+            this.label_score.AutoSize = true;
+            this.label_score.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label_score.Location = new System.Drawing.Point(393, 9);
+            this.label_score.Name = "label_score";
+            this.label_score.Size = new System.Drawing.Size(63, 17);
+            this.label_score.TabIndex = 4;
+            this.label_score.Text = "score : 0";
+            // 
+            // label_sum
+            // 
+            this.label_sum.AutoSize = true;
+            this.label_sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label_sum.Location = new System.Drawing.Point(314, 208);
+            this.label_sum.Name = "label_sum";
+            this.label_sum.Size = new System.Drawing.Size(99, 24);
+            this.label_sum.TabIndex = 1;
+            this.label_sum.Text = "SCORE : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(884, 500);
+            this.Controls.Add(this.label_score);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.slider);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox slider;
+        private System.Windows.Forms.Timer vert;
+        private System.Windows.Forms.Timer horz;
+        private System.Windows.Forms.Timer key_watch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_score;
+        private System.Windows.Forms.Label label_sum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int speed = 9;
+        int vdir = +1;
+        int slider_speed = 10;
+        int score = 0;
+        private void vert_Tick_1(object sender, EventArgs e)
+        {
+            if (ball.Top < 0)
+            {
+                vdir = +1;
+            }
+            else if(ball.Top>(this.Height-ball.Height-slider.Height))
+            {
+
+                if (ball.Left < slider.Left || ((ball.Left + ball.Width) > (slider.Left + slider.Width)))
+                {
+                    vert.Enabled = horz.Enabled = false;
+                    panel1.Visible = true;
+                    panel1.BackColor = Color.Red;
+                    label2.Text = "GAME OVER";
+                    label_sum.Text = "SCORE : "+score;
+                    score = 0;
+                    label_score.Text = "score : " + score;
+                    label_score.Visible = false;
+                }
+                else
+                {
+                    label_score.Visible = true;
+                    score++;
+                    speed+=2;
+                    label_score.Text = "score : " + score;
+
+                }
+
+
+                      vdir = -1;
+            }
+            ball.Top += (vdir*speed);
+        }
+
+        int hdir = +1;
+        private void horz_Tick(object sender, EventArgs e)
+        {
+            if (ball.Top < 0)
+            {
+                hdir = +1;
+            }
+            else if (ball.Left > (this.Width - ball.Width))
+            {
+                hdir = -1;
+            }
+            ball.Left += (hdir * speed);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool _left = false, _right = false; //เลือนช้าย เลื่อนขวา
+        bool game_over = false;
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                _right = true;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                _left = true;
+            }
+            else if (e.KeyCode == Keys.Escape) // กดปุ่ม esc จะหยุด
+            {
+                vert.Enabled = horz.Enabled = pause = false;
+                panel1.BackColor = Color.Green;
+                label2.Text = "GAME Pause";
+            }
+            else if (e.KeyCode == Keys.Enter) // กดปุ่ม enter จะเริ่มต่อ
+            {
+                if (!game_over)
+                {
+
+                }
+                vert.Enabled = horz.Enabled = pause = true;
+            }
+        }
+
+        bool pause
+        {
+            get
+            {
+                return !panel1.Visible;
+            }
+            set
+            {
+                panel1.Visible = !value;
+            }
+        }
+    
+        private void key_watch_Tick(object sender, EventArgs e)
+        {
+             if(_right && slider.Left<(this.Width - slider.Width))
+            {
+                slider.Left =slider.Left + 10;
+            }
+            else if (_left && slider.Left>0)
+            {
+                slider.Left = slider.Left - 10;
+            }
+            
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Left || e.KeyCode == Keys.Right)
+            {
+                _right = _left = false;
+            }
+        }
+    }
+}
